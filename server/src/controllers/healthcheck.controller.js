8c0ba3c9d3a11d77f2cb5f59f384056e924182ab
Tushar Ravaliya <tushar.ravaliya18@gmail.com>
2026-01-27 21:04:21 +0530

    feat(healthcheck): refactor health check to use asyncHandler for improved error handling

--- server/src/controllers/healthcheck.controller.js
+++ server/src/controllers/healthcheck.controller.js
@@ -1,9 +1,8 @@
 import { ApiResponse } from '../utils/api-response.js';
+import { asyncHandler } from '../utils/async-handler.js';
 
-const healthCheck = (req, res) => {
-  try {
-    res.status(200).json(new ApiResponse(200, { message: 'server is running ' }));
-  } catch (eroor) {}
-};
+const healthCheck = asyncHandler(async (req, res) => {
+  res.status(200).json(new ApiResponse(200, { message: 'server is running ' }));
+});
 
 export { healthCheck };

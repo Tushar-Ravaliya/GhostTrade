96da0e244e2b3f7d6474cb19aa0c2ee537b641b4
Tushar Ravaliya <tushar.ravaliya18@gmail.com>
2026-01-27 23:09:10 +0530

    gitkeep

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 
 app.use(
   cors({
-    origin: process.env.CORS_ALLOWED_ORIGINS?.split(',') || 'http://localhost:5174',
+    origin: process.env.CORS_ALLOWED_ORIGINS?.split(',') || 'http://localhost:3000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],

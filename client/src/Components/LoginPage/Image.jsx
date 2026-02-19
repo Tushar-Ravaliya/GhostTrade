6b86e190cec7137aff84fff252e29a46c0046411
KEYURI <keyurijethava@gmail.com>
2026-02-19 22:33:22 +0530

    Make change in login image

---
+++ client/src/Components/LoginPage/Image.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Image() {
+  return (
+    <div className='relative h-full w-full  mx-10'>
+            <img className=' rounded-t-xl -skew-x-12 rounded-b-xl  object-fill ' src="Images/Logo1.jpeg" />
+          </div>
+  )
+}
--- client/src/Pages/login.jsx
+++ client/src/Pages/login.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
+import Image from '../Components/LoginPage/Image'
 
 export default function Login() {
   return (
     <>
-    <div class="bg-[url(/Images/background-image.jpeg)] relative bg-cover flex  h-full   justify-between">
-        <div className='flex justify-start  py-10  bg-black/50 rounded-2xl h-3/4 w-3/4 mx-50 my-20'>
-          <div className='relative h-full w-full mx-10'>
-            <img className=' w-120 rounded-xl  h-auto object-contain' src="Images/Logo1.jpeg" />
+      <div className='bg-[url(/Images/background-image.jpeg)] bg-cover absolute blur-sm items-center   h-full w-full z-0'>
+      </div>
+      <div class=" absolute  flex  h-full z-10 justify-between items-center">
+
+        <div className=' flex justify-start  py-10  bg-black/50 rounded-2xl h-3/4 w-3/4 mx-70 my-20'>
+          <div
+            className="mx-10 my-10 w-full md:w-1/2 shrink-0   rounded-xl h-full flex items-center justify-center "
+            style={{
+              transform: 'skewX(-10deg)', // Skew creates the slant
+              left: '-50%' // Adjust position to hide the left-side skew
+            }}
+          >
+            <Image />
           </div>
-          <div className='flex flex-col gap-4 h-full mx-10 w-full text-white items-center'>
-            <h1 className='text-4xl font-bold'>Login Here</h1>
-            <p className=''>Welcome to paper trading Website </p>
-            <input type="text" className='bg-gray-800 border-1 border-white w-full rounded-lg px-3 py-3'  placeholder='Email'/>
-            <input type="Password" className='bg-gray-800  border-1 border-white w-full rounded-lg px-3 py-3' placeholder='Password'/>
-            <p className='text-m '>Forgot Password</p>
+          <div className='flex flex-col gap-4 h-full mx-10 w-full text-white '>
+            <h1 className='text-4xl font-bold text-center'>Login Here</h1>
+            <p className='text-center'>Welcome to paper trading Website </p>
+            <input type="text" className='bg-gray-800 border-1 border-white w-full rounded-lg px-3 py-3' placeholder='Email' />
+            <input type="Password" className='bg-gray-800  border-1 border-white w-full rounded-lg px-3 py-3' placeholder='Password' />
+            <p className='text-m text-right'>Forgot Password</p>
             <button className='bg-blue-700 rounded-2xl h-auto w-full px-4 py-2'>Login</button>
             <div className='flex gap-60'>
               <p className='text-xs'>I have an Account</p>
@@ -21,7 +31,7 @@ export default function Login() {
             </div>
           </div>
         </div>
-    </div>
+      </div>
     </>
   )
 }

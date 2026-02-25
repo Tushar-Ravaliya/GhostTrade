3d1ad3b0493661a84baa0fb3967f1c4da6609b3d
NananiyaHarshit <nananiyaharshit@gmail.com>
2026-02-25 11:42:47 +0530

    profile changes

--- client/src/Pages/History.jsx
+++ client/src/Pages/History.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function History() {
   return (
     <>
-    <div className='flex h-full w-full justify-center items-center text-white font-bold underline'>
-            History
-        </div>
+      <div className="flex h-full w-full justify-center items-center text-white font-bold underline">
+        History
+      </div>
     </>
-  )
+  );
 }

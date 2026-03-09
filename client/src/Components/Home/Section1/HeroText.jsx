1fe4e5a638a766122f7beef38158bbf437b8c44c
NananiyaHarshit <nananiyaharshit@gmail.com>
2026-03-09 22:38:16 +0530

    Update HeroText styles and adjust Home component layout

--- client/src/Components/Home/Section1/HeroText.jsx
+++ client/src/Components/Home/Section1/HeroText.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const HeroText = () => {
   return (
     <>
-      <div className="py-8 my-16 flex flex-col gap-6 text-white uppercase text-center">
+      <div className="py-8 my-16 flex flex-col gap-6 text-white uppercase text-center font-frijole">
         <div className="tracking-widest text-5xl lg:text-6xl leading-tight px-4">
           Trading without physhical <br />{" "}
           <span className="text-green">trace/Real money.</span>{" "}
         </div>
-        <div className="text-sm tracking-widest font-normal max-w-2xl mx-auto px-6">
+        <div className="text-sm tracking-widest font-normal max-w-2xl text-white/50 mx-auto px-6">
           Connect with collectors worldwide. Buy, sell, and trade authenticated
           banknotes from every era and region with complete security and
           confidence.
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -25,7 +25,7 @@ export default function Home() {
   ];
 
   return (
-    <div className="text-white font-frijole">
+    <div className="text-white ">
       <HeroText />
       <Buttons />
       <div className="flex flex-wrap justify-center gap-36 w-full max-w-5xl mx-auto px-8 py-10">

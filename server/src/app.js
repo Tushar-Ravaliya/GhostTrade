fd47da6bf72afce0396031c938dd4c0b894e1b79
KEYURI <keyurijethava@gmail.com>
2026-03-10 11:38:01 +0530

    Make changes in .nectcore backend

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import healthCheckRouter from './routes/healthcheck.route.js';
 import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+//import cookieParser from 'cookie-parser';
 
 dotenv.config({
   path: './.env',
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
-app.use(cookieParser());
+//app.use(cookieParser());
 
 app.use(
   cors({

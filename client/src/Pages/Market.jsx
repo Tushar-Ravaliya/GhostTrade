365d362ad5366de817570af961ba5c69bccb87d3
KEYURI <keyurijethava@gmail.com>
2026-04-05 22:46:00 +0530

    feat: Refactor Market component structure by correcting import paths and adding Search and Stocks components

--- client/src/Pages/Market.jsx
+++ client/src/Pages/Market.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Search from "../Components/Maeket/Search";
-import Stocks from "../Components/Maeket/Stocks";
+import Search from "../Components/Market/Search";
+import Stocks from "../Components/Market/Stocks";
 
 export default function Market() {
   return (

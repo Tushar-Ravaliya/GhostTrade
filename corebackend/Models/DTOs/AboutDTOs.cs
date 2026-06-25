8e1e2539e366b184fbc7ee11172aa27658e233b4
Tushar Ravaliya <tushar.ravaliya18@gmail.com>
2026-06-25 18:14:53 +0530

    remove dotnet

--- corebackend/Models/DTOs/AboutDTOs.cs
+++
@@ -1,9 +0,0 @@
-namespace corebackend.Models.DTOs
-{
-    public class UpdateAboutRequest
-    {
-        public string? HeroTitle { get; set; }
-        public string? HeroSubtitle { get; set; }
-        public List<Feature>? Features { get; set; }
-    }
-}
